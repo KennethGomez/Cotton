f24ec914a616c67a827f00c2df9d671fc4307891
KennethGomez <kennethgomezfdz@gmail.com>
2022-04-08 03:04:25 +0200

    implement: Console gateway

--- console/src/main.rs
+++ console/src/main.rs
@@ -1,3 +1,17 @@
+use essentials::config::{get_config, ConfigurationProfile};
+use essentials::logger;
+
 fn main() {
-    println!("Hello, world!");
+    // Initialize environment variables from .env file to be accessible from other packages
+    // with the std function std::env()
+    dotenv::dotenv().ok();
+    logger::init(log::LevelFilter::Trace);
+
+    log::info!("reading configuration...");
+
+    let config = get_config(ConfigurationProfile::Environment).unwrap();
+
+    log::info!("initializing server...");
+
+    let _ = server::initialize(&config).unwrap();
 }
